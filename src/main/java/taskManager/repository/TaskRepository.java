9fb6d65da1cfc23df3bdc49d717d9c50784d5596
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-08-23 13:04:56 +0300

    angularjs app

--- src/main/java/taskManager/repository/TaskRepository.java
+++ src/main/java/taskManager/repository/TaskRepository.java
@@ -1,9 +1,13 @@
 package taskManager.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import taskManager.model.Task;
 
+@CrossOrigin
+@RepositoryRestResource(collectionResourceRel = "tasks", path = "tasks")
 public interface TaskRepository extends CrudRepository<Task, Long> {
 
     Iterable<Task> findByCompletedIsNull();
